85cef51d0b7fde34445da15248081c78fab87dd1
gokul nath <gokulaps1998@gmail.com>
2020-09-25 06:47:53 +0530

    user and product models added

--- mongo_routes/users.js
+++ mongo_routes/users.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 //create new user
 router.post("/register", async (req, res) => {
-  const user = new User(user);
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
+  });
   try {
     const newUser = await user.save();
     res.status(200).json({
